bd0edeb0d3ee550d34756633d02d9123f45167a4
Moon-doggie <82822119+Moon-doggie@users.noreply.github.com>
2022-03-26 02:40:37 +1000

    Additional work on admin page front end
    Set up home page and user page including some links

--- moontv/admin/src/App.jsx
+++ moontv/admin/src/App.jsx
@@ -3,15 +3,30 @@ import Sidebar from "./components/sidebar/Sidebar";
 import "./app.scss"
 import Home from "./pages/home/Home";
 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Routes,
+    Route,
+    Navigate
+  } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+
 function App() {
   return (
-    <div>
-        <Navbar/>  
-        <div className="container">
-            <Sidebar/>
-            <Home/>
-        </div>        
-    </div>
+	<Router>
+		<Navbar/>  
+			<div className="container">
+				<Sidebar/>
+            	<Routes>
+					<Route path ="/" element = {<Home/>}/>
+					<Route path ="/users" element = {<UserList/>}/>
+
+           
+                     
+				</Routes>
+            </div>
+      </Router>
   );
 }
 
---
+++ moontv/admin/src/dummyData.js
@@ -0,0 +1,134 @@
+
+    export const userData = [
+        {
+          name: 'Jan',
+          "Active User": 4000,
+        },
+        {
+          name: 'Feb',
+          "Active User": 4500,
+        },
+        {
+          name: 'Mar',
+          "Active User": 3000,
+        },
+        {
+          name: 'Apr',
+          "Active User": 1000,
+        },
+        {
+          name: 'May',
+          "Active User": 2000,
+        },
+        {
+          name: 'Jun',
+          "Active User": 5000,
+        },
+        {
+          name: 'Jul',
+          "Active User": 8000,
+        },
+        {
+          name: 'Aug',
+          "Active User": 4000,
+        },
+        {
+          name: 'Sept',
+          "Active User": 5000,
+        },
+        {
+          name: 'Oct',
+          "Active User": 6000,
+        },
+        {
+          name: 'Nov',
+          "Active User": 2000,
+        },
+        {
+          name: 'Dec',
+          "Active User": 5000,
+        }        
+      ];  
+      
+      export const userRows = [
+        { 
+            id: 1,
+            username: "Jackson Newton",
+            avatar: "https://static.wikia.nocookie.net/evangelion/images/1/12/Rei_Ayanami_OP.png",
+            email: "jacksontest@gmail.com",
+            status: "active",
+            transaction: "$120.00"
+         },
+        { 
+            id: 2,
+            username: "Jackson Newton",
+            avatar: "https://static.wikia.nocookie.net/evangelion/images/1/12/Rei_Ayanami_OP.png",
+            email: "jacksontest@gmail.com",
+            status: "active",
+            transaction: "$120.00"
+         },
+        { 
+            id: 3,
+            username: "Jackson Newton",
+            avatar: "https://static.wikia.nocookie.net/evangelion/images/1/12/Rei_Ayanami_OP.png",
+            email: "jacksontest@gmail.com",
+            status: "active",
+            transaction: "$120.00"
+         },
+        { 
+            id: 4,
+            username: "Jackson Newton",
+            avatar: "https://static.wikia.nocookie.net/evangelion/images/1/12/Rei_Ayanami_OP.png",
+            email: "jacksontest@gmail.com",
+            status: "active",
+            transaction: "$120.00"
+         },
+        { 
+            id: 5,
+            username: "Jackson Newton",
+            avatar: "https://static.wikia.nocookie.net/evangelion/images/1/12/Rei_Ayanami_OP.png",
+            email: "jacksontest@gmail.com",
+            status: "active",
+            transaction: "$120.00"
+         },
+        { 
+            id: 6,
+            username: "Jackson Newton",
+            avatar: "https://static.wikia.nocookie.net/evangelion/images/1/12/Rei_Ayanami_OP.png",
+            email: "jacksontest@gmail.com",
+            status: "active",
+            transaction: "$120.00"
+         },
+        { 
+            id: 7,
+            username: "Jackson Newton",
+            avatar: "https://static.wikia.nocookie.net/evangelion/images/1/12/Rei_Ayanami_OP.png",
+            email: "jacksontest@gmail.com",
+            status: "active",
+            transaction: "$120.00"
+         },
+        { 
+            id: 8,
+            username: "Jackson Newton",
+            avatar: "https://static.wikia.nocookie.net/evangelion/images/1/12/Rei_Ayanami_OP.png",
+            email: "jacksontest@gmail.com",
+            status: "active",
+            transaction: "$120.00"
+         },
+        { 
+            id: 9,
+            username: "Jackson Newton",
+            avatar: "https://static.wikia.nocookie.net/evangelion/images/1/12/Rei_Ayanami_OP.png",
+            email: "jacksontest@gmail.com",
+            status: "active",
+            transaction: "$120.00"
+         },
+        { 
+            id: 10,
+            username: "Jackson Newton",
+            avatar: "https://static.wikia.nocookie.net/evangelion/images/1/12/Rei_Ayanami_OP.png",
+            email: "jacksontest@gmail.com",
+            status: "active",
+            transaction: "$120.00"
+         },
+      ];
